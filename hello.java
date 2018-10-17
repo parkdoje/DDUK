c2a695b1671c36fd38807fcacd9b565b62bd828c
parkdoje <44152370+parkdoje@users.noreply.github.com>
2018-10-17 13:11:42 +0900

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,5 +2,11 @@ class hello
     {
         public static void main (String argv[]){
             System.out.println("hello!");
+            System.out.println("박도제");
+            System.out.println("김찬규");
+            System.out.println("유경읍");
+            System.out.println("구민수");
+
+
         };
     };
